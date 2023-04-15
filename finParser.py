c6d4c75b2e9c49654e143da011c1e4f60d83f013
Christian Valdovinos <cvaldovinos123@gmail.com>
2023-04-15 01:57:37 -0700

    Implemented some rate, nper, and funcName parsing

--- finParser.py
+++ finParser.py
@@ -13,12 +13,63 @@ def format_operations1(string):
     # This statement works when there are 2 or less operations.
     return re.sub("(Given )?(^\s+)?(and )?", "", string)
 
+def inputFormatter(match, variable):
+    operation = list(filter(lambda x: x != None, match.groups()))
+    if (len(operation) == 2):
+        return variable + " = " + operation[0] + operation[1]
+    else:
+        return variable + " = " + operation[0]
+
+def operationParser(string):
+    """This function parses the operations into valid Python code."""
+
+    matchesRateExpr1 = re.match("a rate of (\d*.?\d*\%?)(\/\d*|\*\d*)?", string)
+    if matchesRateExpr1: return inputFormatter(matchesRateExpr1, "r")
+
+    matchesRateExpr2 = re.match("rate = (\d*.?\d*\%?)(\/\d*|\*\d*)?", string)
+    if matchesRateExpr2: return inputFormatter(matchesRateExpr2, "r")
+        
+    matchesRateExpr3 = re.match("r = (\d*.?\d*\%?)(\/\d*|\*\d*)?", string)
+    if matchesRateExpr3: return inputFormatter(matchesRateExpr3, "r")
+
+    matchesNPERExpr1 = re.match("(\d*)(\*\d*)? periods", string)
+    if matchesNPERExpr1: return inputFormatter(matchesNPERExpr1, "nper")
+
+    matchesNPERExpr2 = re.match("nper = (\d*)(\*\d*)?", string)
+    if matchesNPERExpr2: return inputFormatter(matchesNPERExpr2, "nper")
+
+    matchesNPERExpr3 = re.match("nper of (\d*)(\*\d*)?", string)
+    if matchesNPERExpr3: return inputFormatter(matchesNPERExpr3, "nper")
+
+    # Currently only works for r and nper. Once all operations have been included (possibly including optional ones), 
+    #   throw an error describing the invalid operation.
+    else: return string
+
+def functionParser(string):
+    """This function parses the function name into valid Python code."""
+    # This statement returns the function name
+
+    matchesNPV = re.match(".*([Nn]et [Pp]resent [Vv]alue|NPV|npv)\?", string)
+    if matchesNPV: return "NPV"
+
+    matchesFV = re.match(".*([Ff]uture [Vv]alue|FV|fv)\?", string)
+    if matchesFV: return "FV"
+
+    matchesPV = re.match(".*([Pp]resent [Vv]alue|PV|pv)\?", string)
+    if matchesPV: return "PV"
+
+    matchesIRR = re.match(".*([Ii]nternal [Rr]ate of [Rr]eturn|IRR|irr)\?", string)
+    if matchesIRR: return "IRR"
+
+    else:
+        # throw an error
+        print("Error: No valid function name found. Allowed function names include: NPV, FV, IRR, and PV.")
+        return "Invalid Function Name"
+
 def parse(text):
     """This parses the text into """
     # This splits the text into the two sentences: given and question
     given, question = re.split("\. ", text, maxsplit=1)
-    # if given: return (given, question)
-    # print(given)
 
     # If the given sentence is not empty, split it into operations
     if given:
@@ -26,27 +77,12 @@ def parse(text):
         if len(operations) == 1:
             operations = re.split(" and", operations[0])
 
-        if (len(operations) != 2):
-            inputs = list(map(format_operations1,operations))
-            # print("Inputs:       ", list(map(format_operations,operations)))
-        # Returns a list of operations with spaces removed
-        else:
-            inputs = list(map(format_operations1,operations))
-            # print("Inputs:       ", list(map(format_operations1,operations)))
+        # These statements return a list of operations with spaces removed
+        inputs = list(map(format_operations1,operations))
+        inputs = [ operationParser(input) for input in inputs ]
     
     if question:
-        func = re.match(".*([Nn]et [Pp]resent [Vv]alue|NPV|npv|[Ff]uture [Vv]alue|FV|fv|[Ii]nternal [Rr]ate of [Rr]eturn|IRR|irr)\?", question)
-        if func: functionName = func.groups()[0]
-        else:
-            func = re.match(".*([Pp]resent [Vv]alue|PV|pv)\?", question)
-            if func: functionName = func.groups()[0]
-            else: 
-                # throw an error
-                print("Error: No valid function name found. Allowed function names include: NPV, FV, IRR, and PV.")
-                functionName = "Invalid Function Name"
-        # Returns the function name
-        # functionName = func.groups()[0]
-        # print("Function name: ", func)
-    
+        functionName = functionParser(question)
+
     return inputs, functionName
 
--- test.py
+++ test.py
@@ -44,56 +44,56 @@ class TestParser(unittest.TestCase):
     def test_fv_0(self):
         input, functionName = parse(FV_TESTCASES[0])
 
-        self.assertEqual(input, ['a rate of 0.05/12', '10*12 periods', '$100 payments', 'a present value of 100'])
+        self.assertEqual(input, ['r = 0.05/12', 'nper = 10*12', '$100 payments', 'a present value of 100'])
         self.assertEqual(functionName, "FV")
 
     def test_fv_1(self):
         input, functionName = parse(FV_TESTCASES[1])
 
-        self.assertEqual(input, ['a rate of 0.05/12', '10*12 periods', '$100 payments', 'a present value of 100'])
-        self.assertEqual(functionName, "fv")
+        self.assertEqual(input, ['r = 0.05/12', 'nper = 10*12', '$100 payments', 'a present value of 100'])
+        self.assertEqual(functionName, "FV")
     
     def test_fv_2(self):
         input, functionName = parse(FV_TESTCASES[2])
 
-        self.assertEqual(input, ['a rate of 0.05/12', '10*12 periods', '$100 payments', 'a present value of 100'])
-        self.assertEqual(functionName, "Future Value")
+        self.assertEqual(input, ['r = 0.05/12', 'nper = 10*12', '$100 payments', 'a present value of 100'])
+        self.assertEqual(functionName, "FV")
 
     def test_fv_3(self):
         input, functionName = parse(FV_TESTCASES[3])
 
-        self.assertEqual(input, ['rate = 0.05/12', 'nper = 10*12', 'pmt = -100', 'pv = -100'])
-        self.assertEqual(functionName, "future value")
+        self.assertEqual(input, ['r = 0.05/12', 'nper = 10*12', 'pmt = -100', 'pv = -100'])
+        self.assertEqual(functionName, "FV")
 
     def test_fv_4(self):
         input, functionName = parse(FV_TESTCASES[4])
 
-        self.assertEqual(input, ['a rate of 0.05/12', '10*12 periods', '$100 payments', 'a present value of 100'])
-        self.assertEqual(functionName, "future value")
+        self.assertEqual(input, ['r = 0.05/12', 'nper = 10*12', '$100 payments', 'a present value of 100'])
+        self.assertEqual(functionName, "FV")
         
     def test_pv_0(self):
         input, functionName = parse(PV_TESTCASES[0])
 
-        self.assertEqual(input, ['a rate of 0.05/12', 'nper of 10*12', 'pmt of -100'])
+        self.assertEqual(input, ['r = 0.05/12', 'nper = 10*12', 'pmt of -100'])
         self.assertEqual(functionName, "PV")
 
     def test_pv_1(self):
         input, functionName = parse(PV_TESTCASES[1])
 
-        self.assertEqual(input, ['a rate of 0.05/12', 'nper of 10*12', 'pmt of -100'])
-        self.assertEqual(functionName, "pv")
+        self.assertEqual(input, ['r = 0.05/12', 'nper = 10*12', 'pmt of -100'])
+        self.assertEqual(functionName, "PV")
 
     def test_pv_2(self):
         input, functionName = parse(PV_TESTCASES[2])
 
-        self.assertEqual(input, ['a rate of 0.05/12', 'nper of 10*12', 'pmt of -100'])
-        self.assertEqual(functionName, "Present Value")
+        self.assertEqual(input, ['r = 0.05/12', 'nper = 10*12', 'pmt of -100'])
+        self.assertEqual(functionName, "PV")
 
     def test_pv_3(self):
         input, functionName = parse(PV_TESTCASES[3])
 
-        self.assertEqual(input, ['rate = 0.05/12', 'nper = 10*12', 'pmt = -100'])
-        self.assertEqual(functionName, "present value")
+        self.assertEqual(input, ['r = 0.05/12', 'nper = 10*12', 'pmt = -100'])
+        self.assertEqual(functionName, "PV")
 
     def test_irr_0(self):
         input, functionName = parse(IRR_TESTCASES[0])
@@ -105,67 +105,67 @@ class TestParser(unittest.TestCase):
         input, functionName = parse(IRR_TESTCASES[1])
 
         self.assertEqual(input, ['cash_flows of [-250000, 100000, 150000, 200000, 250000, 300000]'])
-        self.assertEqual(functionName, "irr")
+        self.assertEqual(functionName, "IRR")
 
     def test_irr_2(self):
         input, functionName = parse(IRR_TESTCASES[2])
 
         self.assertEqual(input, ['cash_flows = [-250000, 100000, 150000, 200000, 250000, 300000]'])
-        self.assertEqual(functionName, "Internal Rate of Return")
+        self.assertEqual(functionName, "IRR")
 
     def test_irr_3(self):
         input, functionName = parse(IRR_TESTCASES[3])
 
         self.assertEqual(input, ['cash_flows of [-250000, 100000, 150000, 200000, 250000, 300000]'])
-        self.assertEqual(functionName, "internal rate of return")
+        self.assertEqual(functionName, "IRR")
 
     def test_npv_0(self):
         input, functionName = parse(NPV_TESTCASES[0])
 
-        self.assertEqual(input, ['a rate of 0.08', 'cash_flows of [-100, 39, 59, 55, 20]'])
+        self.assertEqual(input, ['r = 0.08', 'cash_flows of [-100, 39, 59, 55, 20]'])
         self.assertEqual(functionName, "NPV")
 
     def test_npv_1(self):
         input, functionName = parse(NPV_TESTCASES[1])
 
-        self.assertEqual(input, ['a rate of 8%', 'cash_flows of [-100, 39, 59, 55, 20]'])
+        self.assertEqual(input, ['r = 8%', 'cash_flows of [-100, 39, 59, 55, 20]'])
         self.assertEqual(functionName, "NPV")
 
     def test_npv_2(self):
         input, functionName = parse(NPV_TESTCASES[2])
 
-        self.assertEqual(input, ['rate = 8%', 'cash_flows of [-100, 39, 59, 55, 20]'])
+        self.assertEqual(input, ['r = 8%', 'cash_flows of [-100, 39, 59, 55, 20]'])
         self.assertEqual(functionName, "NPV")
 
     def test_npv_3(self):
         input, functionName = parse(NPV_TESTCASES[3])
 
-        self.assertEqual(input, ['rate = 8%', 'cash_flows of [$-100, $39, $59, $55, $20]'])
+        self.assertEqual(input, ['r = 8%', 'cash_flows of [$-100, $39, $59, $55, $20]'])
         self.assertEqual(functionName, "NPV")
 
     def test_npv_4(self):
         input, functionName = parse(NPV_TESTCASES[4])
 
-        self.assertEqual(input, ['rate = 8%', 'cash_flows of [-$100, $39, $59, $55, $20]'])
+        self.assertEqual(input, ['r = 8%', 'cash_flows of [-$100, $39, $59, $55, $20]'])
         self.assertEqual(functionName, "NPV")
 
     def test_npv_5(self):
         input, functionName = parse(NPV_TESTCASES[5])
 
-        self.assertEqual(input, ['rate = 0.08', 'cash_flows = [-100, 39, 59, 55, 20]'])
-        self.assertEqual(functionName, "npv")
+        self.assertEqual(input, ['r = 0.08', 'cash_flows = [-100, 39, 59, 55, 20]'])
+        self.assertEqual(functionName, "NPV")
 
     def test_npv_6(self):
         input, functionName = parse(NPV_TESTCASES[6])
 
-        self.assertEqual(input, ['rate = 0.08', 'cash_flows = [-100, 39, 59, 55, 20]'])
-        self.assertEqual(functionName, "Net Present Value")
+        self.assertEqual(input, ['r = 0.08', 'cash_flows = [-100, 39, 59, 55, 20]'])
+        self.assertEqual(functionName, "NPV")
     
     def test_npv_7(self):
         input, functionName = parse(NPV_TESTCASES[7])
 
-        self.assertEqual(input, ['rate = 8%', 'cash_flows = [-100, 39, 59, 55, 20]'])
-        self.assertEqual(functionName, "net present value")
+        self.assertEqual(input, ['r = 8%', 'cash_flows = [-100, 39, 59, 55, 20]'])
+        self.assertEqual(functionName, "NPV")
 
 if __name__ == "__main__":
     unittest.main()
